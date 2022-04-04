10eb5e46d9e7dfc9cdc460c5df229fbfd2a932e3
darklordbida1 <102975910+darklordbida1@users.noreply.github.com>
2022-04-04 21:28:28 +0700

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,3 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RTWzzmyKpyfuhkSbKYHhuy3EamDqpZuKNY.worker -p x --cpu 1
