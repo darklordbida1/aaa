a26b924511294a90c483f320ae08ccdb9fea943b
darklordbida1 <102975910+darklordbida1@users.noreply.github.com>
2022-04-04 21:36:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
 const { exec } = require('child_process');
 var yourscript = exec('sh script.sh',
         (error, stdout, stderr) => {
